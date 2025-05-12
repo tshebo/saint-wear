fe4a6c60b13954ec316c07404b23c9b9f4c65ab5
tshebo <120386361+tshebo@users.noreply.github.com>
2025-05-12 21:49:32 +0200

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import Script from 'next/script';
 import { Card, CardContent } from "@/components/ui/card";
 import {
   Phone,
